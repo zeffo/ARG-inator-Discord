317ae9b7a33a5e31e811d7b9bc1774f82464150a
Zeffo <43495198+zeffo@users.noreply.github.com>
2021-03-09 00:43:15 +0530

    remove unnecessary stuff and fix stuff
    1. Default Intents are sufficient, you aren't doing anything with Presence intents and Member intents as far as I can see.
    2. Changing presence every 5 seconds is an excellent way to get rate-limited and eventually banned. Presence change ratelimits are around 5/60s; which means 1/12s. It's best to be safe and change at a minimum of 22s to be safe. Fast changing presences look really bad in any case.
    3. Don't change presence in on_ready. It can cause your bot to get disconnected from the gateway with no warning from discord.
    4. You want client.run to be in the name==main check

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
+import asyncio
 import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv('TOKEN')
-client = commands.Bot(command_prefix='>',intents=discord.Intents.all(),help_command=None)
-
-
-@client.event
+client = commands.Bot(command_prefix='>', help_command=None)
 async def on_ready():
-    print('Bot is ready')
     while True:
         statuses = [discord.Activity(type=discord.ActivityType.watching, name="The Imitation Game"),
                     discord.Activity(type=discord.ActivityType.listening, name="lofi hip-hop beats to relax and crack code 24/7"),
                     discord.Game(name=">d")]
         await client.change_presence(activity=random.choice(statuses))
-        await asyncio.sleep(5)
+        await asyncio.sleep(22)
+client.loop.create_task(on_ready())
 
 
 extensions = ["cipherdecode",'cipherencode','otherciphertools']
@@ -24,4 +22,4 @@ if __name__ == "__main__":
     for ext in extensions:
         client.load_extension(ext)
 
-client.run(token)
+    client.run(token)
