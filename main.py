a4815f195ea767063fe42828ebd4aef9491bf637
Tamim Anaf <48618923+Thuliumify@users.noreply.github.com>
2021-03-08 22:55:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ client = commands.Bot(command_prefix='>',intents=discord.Intents.all(),help_comm
 async def on_ready():
     print('Bot is ready')
     while True:
-    statuses = [discord.Activity(type=discord.ActivityType.watching, name="The Imitation Game"),
-                discord.Activity(type=discord.ActivityType.listening, name="lofi hip-hop beats to relax and crack code 24/7"),
-                discord.Game(name=">d")]
-    await client.change_presence(activity=random.choice(statuses))
-    await asyncio.sleep(5)
+        statuses = [discord.Activity(type=discord.ActivityType.watching, name="The Imitation Game"),
+                    discord.Activity(type=discord.ActivityType.listening, name="lofi hip-hop beats to relax and crack code 24/7"),
+                    discord.Game(name=">d")]
+        await client.change_presence(activity=random.choice(statuses))
+        await asyncio.sleep(5)
 
 
 extensions = ["cipherdecode",'cipherencode','otherciphertools']
