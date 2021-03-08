3979dc51325d9a842b8f11b452de750b1f040aa0
Tamim Anaf <48618923+Thuliumify@users.noreply.github.com>
2021-03-08 20:25:58 +0600

    New morse code meaning added

--- resources.py
+++ resources.py
@@ -15,15 +15,6 @@ morse_dict = {' ': '/',', ': '--..--', '.': '.-.-.-',
               '6': '-....', '7': '--...', '8': '---..',
               '9': '----.','0': '-----'}
 
-def brute_force(str_to_brute_with, str_to_crack):
-    complete_list = []
-    for current in range(2, 3):
-        a = [i for i in str_to_crack]
-        for y in range(current):
-            a = [x + i for i in str_to_brute_with for x in a]
-        complete_list += a
-    return complete_list
-
 #Just to get the modinverse for affine cipher, gave it here cuz i mean its a resource tho
 def invmod(a, m):
     for x in range(1, m):
